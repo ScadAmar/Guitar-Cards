bbda4090b691354cb52b39b12ca2ec2da3610ee8
ScadAmar <scadamar@gmail.com>
2024-08-16 18:36:27 +0800

    Deleted hero page

--- guitar-cards/src/App.jsx
+++ guitar-cards/src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import CardModule from "./Sections/Cards/CardModule";
 import Contact from "./Sections/Contact/Contact";
 import Footer from "./Sections/Footer/Footer";
-import Hero from "./Sections/Hero/Hero";
+//import Hero from "./Sections/Hero/Hero";
 import LearnMore from "./Sections/Learn-More/LearnMore";
+import NavBar from "./Sections/Navigation-Bar/NavBar";
 
 function App() {
   return (
     <>
-      <Hero />
+      <NavBar />
       <CardModule />
       <LearnMore />
       <Contact />
